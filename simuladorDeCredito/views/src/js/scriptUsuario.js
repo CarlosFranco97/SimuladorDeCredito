fc78cd6538121c09b5ea941fd8b6197ce0b8dbbf
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-11-25 14:07:53 -0500

    Se realizan cambio en el encarpetado

--- simuladorDeCredito/views/src/js/scriptUsuario.js
+++
@@ -1,5 +0,0 @@
-import { login, register } from "../../../controllers/ControllerUsuario.js";
-
-document.getElementById('btnRegistrar').addEventListener('click', register); 
-document.getElementById('btnLogin').addEventListener('click', login);
-
